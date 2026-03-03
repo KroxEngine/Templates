1dbe1d7ee2dcb09df03160d241f0fe724dff7fb1
ZolanPro <ZolanPro@gmail.com>
2026-03-03 20:27:47 +0100

    Обновил README.md, убрал тестовый шаблон, настроил .gitignore для шаблонных папок

--- Three-test/src/Views/InstallButton.ts
+++
@@ -1,9 +0,0 @@
-import { RegisterView, ThreeSprite } from '@krox/three';
-import { AdAPI } from '../../../../packages/adapi';
-
-@RegisterView('InstallButton')
-export default class InstallButton extends ThreeSprite {
-  install() {
-    AdAPI.openStore();
-  }
-}
